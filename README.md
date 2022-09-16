51045bef32aa97397bc12d6d28f9a8a876687e32
meXuvo <rafiqulislamsuvobd@gmail.com>
2022-09-16 23:35:04 +0600

    run hello world program

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learn JAVA
+
+## Day 1
+
+- [Run hello world program] (https://github.com/mexuvo)
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        // write code here
+        System.out.println("Hello world");
+    }
+}
