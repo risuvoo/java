c8b42e7fbdf615983046b8532a418b82c43bacdf
meXuvo <rafiqulislamsuvobd@gmail.com>
2022-09-19 00:28:42 +0600

    print personal info

---
+++ src/PrintBasicInfo.java
@@ -0,0 +1,11 @@
+public class PrintBasicInfo {
+    public static void main(String[] args){
+        String name= "Rafiqul Islam Suvo";
+        String id= "202211062065";
+        String email="rafiqulislamsuvobd@gmail.com";
+
+        System.out.println("Name : "+name);
+        System.out.println("ID : "+id);
+        System.out.println("Email : "+email);
+    }
+}
